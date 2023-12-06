fcd4f0439493d038fc4062d0bae95ed79323849c
Can Dogrul <candogrull@hotmail.com>
2023-12-06 16:50:26 +0100

    modified:   Lab5/Lab5_1.c

--- Lab5/Lab5_1.c
+++ Lab5/Lab5_1.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int perm(int *val, int *sol, int *mark,
+         int n, int count, int pos);
+int magicsquare();
+void writeMagicSquare();
+
+int main()
+{
+    return 0;
+}
+
+int perm(int *val, int *sol, int *mark,
+         int n, int count, int pos)
+{
+    int i;
+
+    val = malloc(n * sizeof(int));
+    sol = malloc(n * sizeof(int));
+    mark = malloc(n * sizeof(int));
+    
+    if (pos >= n)
+    {
+        return count + 1;
+    }
+}
