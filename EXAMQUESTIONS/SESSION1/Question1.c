79a66df173c552045d6796b7870d984e1ad36fa2
Can Dogrul <candogrull@hotmail.com>
2023-12-07 17:10:09 +0100

    new file:   EXAMQUESTIONS/.vscode/tasks.json 	new file:   EXAMQUESTIONS/SESSION1/Question1 	modified:   EXAMQUESTIONS/SESSION1/Question1.c 	new file:   EXAMQUESTIONS/SESSION1/Question1.dSYM/Contents/Info.plist 	new file:   EXAMQUESTIONS/SESSION1/Question1.dSYM/Contents/Resources/DWARF/Question1

--- EXAMQUESTIONS/SESSION1/Question1.c
+++ EXAMQUESTIONS/SESSION1/Question1.c
@@ -2,19 +2,37 @@
 #include <stdlib.h>
 
 int *merge3(int *a, int *b, int *c, int na,
-            int nb, int nc);
+            int nb, int nc, int *n);
 int main()
 {
+
+    int *finalArray;
+    int n;
+    int a[] = {1, 4, 10, 11};
+    int b[] = {2, 5, 6, 12, 24};
+    int c[] = {0, 7, 8};
+
+    finalArray = merge3(a, b, c, sizeof(a), sizeof(b),
+                        sizeof(c), &n);
+
+    for (int i = 0; i < sizeof(finalArray); i++)
+    {
+        fprintf(stdout, "%i \n", finalArray[i]);
+        }
+
+    fprintf(stdout, "%ld", sizeof(finalArray));
 
     return 0;
 }
 
 int *merge3(int *a, int *b, int *c, int na,
-            int nb, int nc)
+            int nb, int nc, int *n)
 {
     int *d, *m[3], len[3];
     int tot = na + nb + nc;
     int i, j;
+
+
 
     m[0] = a;
     m[1] = b;
@@ -29,12 +47,12 @@ int *merge3(int *a, int *b, int *c, int na,
     {
         exit(EXIT_FAILURE);
     }
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < tot; i++)
     {
         int min_index = -1;
         for (j = 0; j < 3; j++)
         {
-            if (len[j] != 0 || (*m[min_index] < *m[j]))
+            if (len[j] != 0 && (min_index == -1 || *m[min_index] > *m[j]))
             {
                 min_index = j;
             }
@@ -43,6 +61,8 @@ int *merge3(int *a, int *b, int *c, int na,
         m[min_index]++;
         len[min_index]--;
     }
+
+    *n = tot;
 
     return d;
 }
