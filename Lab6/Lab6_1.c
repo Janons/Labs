dfc5ca0b0c1cd360e3c0a8b7c1ce8b6f67234aca
Can Dogrul <candogrull@hotmail.com>
2023-12-13 17:55:34 +0100

    new file:   Lab6/Lab6_1.c 	new file:   Lab6/Lab6_1_pseudo.txt 	new file:   Lab6/Maze.txt 	deleted:    Lab6/lab06-StackLibrary.zip

---
+++ Lab6/Lab6_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*prototypes*/
+
+void **matrixCreate(int **matrix, int r, int c,char *name);
+void my_memoryAllocation(int **matrix, int r);
+int recursion();
+
+int main()
+{
+    return 0;
+}
