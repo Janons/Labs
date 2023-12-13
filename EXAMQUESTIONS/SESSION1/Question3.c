0b4b9a22e4414beb667df84f7cdf1391a9f4efdf
Can Dogrul <candogrull@hotmail.com>
2023-12-13 12:16:39 +0100

    modified:   .DS_Store 	new file:   EXAMQUESTIONS/SESSION1/Question3.c 	new file:   Lab5/.idea/.gitignore 	new file:   Lab5/.idea/Lab5.iml 	new file:   Lab5/.idea/modules.xml 	new file:   Lab5/.idea/vcs.xml 	new file:   Lab5/.idea/workspace.xml 	new file:   Lab5/Lab5__1.c

---
+++ EXAMQUESTIONS/SESSION1/Question3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int backtrack()
+{
+}
+
+int main()
+{
+    return 0;
+}
---
+++ Lab5/Lab5__1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*prototypes*/
+int backtrack(int n, int array[]);
+int basecaseCheck();
+
+
+int main(int argc, char * argv)
+{
+    if (argc>)
+    return 0;
+}
+
