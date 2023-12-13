e7265d327c75c85f0cd110f33878d2da79dcfce9
Can Dogrul <candogrull@hotmail.com>
2023-12-13 12:50:41 +0100

    modified:   Lab5/Lab5_1.c 	deleted:    Lab5/Lab5__1.c

--- Lab5/Lab5_1.c
+++ Lab5/Lab5_1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void matrixPerm(int **matrix, int *val, int *sol, int *mark,
+void matrixPerm(int **matrix, int *matrixArray, int *val, int *sol, int *mark,
                 int n, int count, int pos, int magicnumber);
 bool checkMagicSquare(int **matrix, int n, int magicNumber);
 void writeMagicSquare();
@@ -11,25 +11,36 @@ void freeThematrix();
 int main(int argc, char *argv[])
 {
     int magicNumber;
-    int **matrix;
+    int **matrix, count = 1, i, j;
 
     if (argc < 3)
     {
         return EXIT_SUCCESS;
     }
     int n = (int)argv[2];
+    int *matrixArray;
     magicNumber = n * ((n * n) + 1) / 2;
 
     /*dynamic memory allocation for the matrix*/
     **matrix = (int **)malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            matrix[i][j] = 0;
+            matrix[i][j] = count;
+            count++;
         }
     }
 
+    /*matrix array*/
+    *matrixArray = (int *)malloc(n * n * sizeof(int));
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            matrixArray[(n * i) + j] = matrix[i][j];
+        }
+    }
     return 0;
 }
 
@@ -82,19 +93,30 @@ bool checkMagicSquare(int **matrix, int n, int magicNumber)
     return false;
 }
 
-void matrixPerm(int **matrix, int *val, int *sol, int *mark,
+void matrixPerm(int **matrix, int *matrixArray, int *val, int *sol, int *mark,
                 int n, int count, int pos, int magicnumber)
 {
     int i;
 
-    val = (int *)malloc(n * sizeof(int));
-    sol = (int *)malloc(n * sizeof(int));
-    mark = (int *)calloc(n, sizeof(int));
-
-    if (checkMagicSquare(matrix, n, magicnumber))
+    if (pos >= n * n)
         return;
+
+    *mark = calloc(n * n, sizeof(int));
+    *sol = (int *)malloc(n * n * sizeof(int));
+
+    for (i = 0; i < n * n; i++)
+    {
+        if (mark[i] == 0)
+        {
+            mark[i] = 1;
+
+            sol[pos] = matrixArray[i];
+            
+        }
+    }
 }
 
 /*To-Do
 1) Write the same code with calloc
-2)Write the same code with lesser lines*/
+2)Write the same code with lesser lines
+3)Go over dynamic memory allocation once again,master the logic*/
