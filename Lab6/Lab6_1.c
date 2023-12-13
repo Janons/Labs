fffcc0accfc557a4d8c1a58ac003fc093cf886ca
Can Dogrul <candogrull@hotmail.com>
2023-12-13 18:18:22 +0100

    modified:   Lab6/Lab6_1.c 	new file:   Lab6/smallMaze.txt

--- Lab6/Lab6_1.c
+++ Lab6/Lab6_1.c
@@ -3,11 +3,46 @@
 
 /*prototypes*/
 
-void **matrixCreate(int **matrix, int r, int c,char *name);
+void **matrixCreate(int **matrix, int *nr, int *nc, char *name);
 void my_memoryAllocation(int **matrix, int r);
 int recursion();
 
 int main()
 {
     return 0;
+}
+
+void **matrixCreate(int **matrix, int *nr, int *nc, char *name)
+{
+    /*variable*/
+    FILE *fmaze;
+    int i, j;
+    int r, c;
+
+    /*open the file*/
+    fmaze = fopen(name, "r");
+    if (fmaze == NULL)
+    {
+        fprintf(stderr, "Error in parsing the file");
+    }
+
+    fscanf(fmaze, "%d %d", &r, &c);
+    char line[c];
+
+    /*matrix creation*/
+    while (fgets(line, c, fmaze) == NULL)
+    {
+        j = 0;
+        while (j < c && (line[j] == ' ' || line[j] == '*' || line[j] == '#'))
+        {
+            if (matrix[i][j] == '*')
+                matrix[i][j] = 1;
+            j++;
+        }
+        i++;
+    }
+    fclose(fmaze);
+
+    *nr = r;
+    *nc = c;
 }
