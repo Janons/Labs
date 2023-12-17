19f7b34dccbdc2b7938e3aaf71ad90b0cea2bb68
Can Dogrul <candogrull@hotmail.com>
2023-12-17 18:07:55 +0100

    new file:   EXAMQUESTIONS/Session2/Session2_2.c 	new file:   EXAMQUESTIONS/Session2/Session_2_2.h

---
+++ EXAMQUESTIONS/Session2/Session_2_2.h
@@ -0,0 +1,27 @@
+#ifndef _SESSION_2_2
+#define _SESSION_2_2
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node_s
+{
+    int col;
+    float f;
+    struct node_s *next;
+} node_t;
+
+typedef struct list_s
+{
+    int ne;
+    node_t *next;
+} list_t;
+
+typedef struct matr_s
+{
+    int nr;
+    int nc;
+    list_t *rows;
+} matr_t;
+
+#endif
