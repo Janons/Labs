afc806f754ae6db592dc65678cc647474b397977
Can Dogrul <candogrull@hotmail.com>
2023-12-17 17:43:49 +0100

    modified:   EXAMQUESTIONS/SESSION1/Question1.c

--- EXAMQUESTIONS/SESSION1/Question1.c
+++ EXAMQUESTIONS/SESSION1/Question1.c
@@ -1,68 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *merge3(int *a, int *b, int *c, int na,
-            int nb, int nc, int *n);
+int *merge3(int *a, int *b, int *c, int na, int nb, int nc);
 int main()
 {
 
-    int *finalArray;
-    int n;
-    int a[] = {1, 4, 10, 11};
-    int b[] = {2, 5, 6, 12, 24};
-    int c[] = {0, 7, 8};
-
-    finalArray = merge3(a, b, c, sizeof(a), sizeof(b),
-                        sizeof(c), &n);
-
-    for (int i = 0; i < sizeof(finalArray); i++)
-    {
-        fprintf(stdout, "%i \n", finalArray[i]);
-        }
-
-    fprintf(stdout, "%ld", sizeof(finalArray));
-
-    return 0;
 }
 
-int *merge3(int *a, int *b, int *c, int na,
-            int nb, int nc, int *n)
+int *merge3(int *a, int *b, int *c, int na, int nb, int nc)
 {
-    int *d, *m[3], len[3];
-    int tot = na + nb + nc;
-    int i, j;
-
-
+    int *d;
+    int *m[3], len[3];
 
     m[0] = a;
     m[1] = b;
     m[2] = c;
+
     len[0] = na;
     len[1] = nb;
     len[2] = nc;
 
-    /*dynamic memory allocation*/
-    d = (int *)malloc(tot * sizeof(int));
+    int sum = na + nb + nc;
+
+    d = (int *)malloc(sum * sizeof(int));
     if (d == NULL)
     {
-        exit(EXIT_FAILURE);
+        perror("Error in memory allocating");
+        return EXIT_FAILURE;
     }
-    for (i = 0; i < tot; i++)
+
+    for (int i; i < sum; i++)
     {
         int min_index = -1;
-        for (j = 0; j < 3; j++)
+        for (int j = 0; j < 3; j++)
         {
-            if (len[j] != 0 && (min_index == -1 || *m[min_index] > *m[j]))
+            if (len[j] != 0 || (min_index == -1 || *m[min_index] > *m[j]))
             {
                 min_index = j;
             }
         }
-        d[i] = *m[min_index];
+
+        d[i] = *m[min_index]; // we access the value with the pointers
         m[min_index]++;
         len[min_index]--;
     }
-
-    *n = tot;
 
     return d;
 }
