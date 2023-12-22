6a950935214fcde73bd2537f2c3c30cc8bbde3ed
Can Dogrul <candogrull@hotmail.com>
2023-12-22 11:56:46 +0300

    modified:   Lab6/Lab6_1.c

--- Lab6/Lab6_1.c
+++ Lab6/Lab6_1.c
@@ -1,132 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
-/*definition*/
+#define MAX 100
+
 #define EMPTY ' '
 #define START '@'
 #define STOP '#'
 #define PATH '$'
 
-const int xOff = {0, 1, 0, -1};
-const int yOff = {-1, 0, 1, 0};
+const int xOff[4] = {0, 1, 0, -1};
+const int yOff[4] = {-1, 0, 1, 0};
 
-/*prototypes*/
-
-void **matrixCreate(int **matrix, int *nr, int *nc, char *name);
-int *my_memoryAllocation(int size, int typesize);
-bool isValid(int **matrix, int r, int c, int x, int y);
-
-void recursion(int **matrix, int x, int y, int *path);
+/* function prototypes */
+void display(char **maze, int nr);
+int move_recur(char **mazeCurr, int stepCurr, char **mazeBest, int stepBest,
+               int nr, int nc, int row, int col);
+FILE *util_fopen(char *name, char *mode);
+void *util_malloc(int size);
+char *util_strdup(char *src);
 
 int main(int argc, char *argv)
 {
     int **matrix;
-    int nr, nc;
-
-    matrixCreate(matrix, nr, nc, argv[1]);
-
-    int *path;
-
-    path = (int *)malloc(nr * nc * sizeof(int));
-    if (path == NULL)
-    {
-        return EXIT_FAILURE;
-    }
-
-    return 0;
 }
 
-int *my_memoryAllocation(int size, int typesize)
+void *util_malloc(int size)
 {
-    int mem = malloc(size * sizeof(typesize));
+    int *mem;
+    mem = malloc(size);
+
     if (mem == NULL)
     {
-        exit(-1);
+        fprintf(stderr, "Error in memory allocation 1");
+        exit(EXIT_FAILURE);
     }
+
     return mem;
 }
 
-void **matrixCreate(int **matrix, int *nr, int *nc, char *name)
+char *util_strdup(char *src)
 {
-    /*variable*/
-    FILE *fmaze;
-    int i, j;
-    int r, c;
-
-    /*open the file*/
-    fmaze = fopen(name, "r");
-    if (fmaze == NULL)
+    src = strdup(src);
+    if (src == NULL)
     {
-        fprintf(stderr, "Error in parsing the file");
+        fprintf(stderr, "Error in memory allocation 2");
+        exit(EXIT_FAILURE);
     }
-
-    fscanf(fmaze, "%d %d", &r, &c);
-    char line[c];
-
-    /*memory allocation for the matrix*/
-    matrix = (int **)my_memoryAllocation(r, sizeof(int *));
-    for (i = 0; i < r; i++)
-    {
-        matrix[i] = (int *)my_memoryAllocation(c, sizeof(int));
-    }
-
-    /*matrix creation*/
-    while (fgets(line, c, fmaze) == NULL)
-    {
-        j = 0;
-        while (j < c && (line[j] == ' ' || line[j] == '*' || line[j] == '#'))
-        {
-            if (matrix[i][j] == ' ')
-                matrix[i][j] = -1;
-            j++;
-        }
-        i++;
-    }
-    fclose(fmaze);
-
-    *nr = r;
-    *nc = c;
 }
 
-bool isValid(int **matrix, int r, int c, int x, int y)
+FILE *util_fopen(char *name, char *mode)
 {
-    /*row,col check*/
+    FILE *fin;
+    fin = fopen(name, mode);
 
-    bool row = (x <= c && y <= c) ? true : false;
-    bool result = (matrix[x][y] != "*") ? true : false;
-
-    if (row && result)
-        return true;
-
-    return false;
-}
-
-/*finding a general solution*/
-void recursion(int **matrix, int x, int y, int *path);
-{
-
-    int xoffset = {0, 1, 0, -1};
-    int yoffset = {1, 0, -1, 0};
-    int xx, yy;
-
-    if (map[x][y] == "#")
+    if (fin == NULL)
     {
-
-        return;
-    }
-
-    /*mark the position*/
-    map[x][y] = '.';
-
-    /*look for a path to escape*/
-    for (i = 0; i < 4; i++)
-    {
-        xx = x + xoff[i];
-        yy = y + yoff[i];
-
-        if (isValid(xx, yy, matrix))
-        {
-        }
+        fprintf(stderr, "Error in file parsing");
+        exit(EXIT_FAILURE);
     }
 }
+
+int move_recur(char **mazeCurr, int stepCurr, char **mazeBest, int stepBest,
+               int nr, int nc, int row, int col)
+{
+
+}
+
+
+
+
+/*Writing the professors solution once again on my own*/
