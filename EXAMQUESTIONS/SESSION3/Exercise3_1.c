e9bf3a1ce5050b748f42267782a8d8c491cc3475
Can Dogrul <candogrull@hotmail.com>
2023-12-27 19:23:44 +0300

    new file:   EXAMQUESTIONS/SESSION3/Exercise1 	new file:   EXAMQUESTIONS/SESSION3/Exercise1.dSYM/Contents/Info.plist 	new file:   EXAMQUESTIONS/SESSION3/Exercise1.dSYM/Contents/Resources/DWARF/Exercise1 	new file:   EXAMQUESTIONS/SESSION3/Exercise3_1 	renamed:    EXAMQUESTIONS/SESSION3/Exercise1.c -> EXAMQUESTIONS/SESSION3/Exercise3_1.c

--- EXAMQUESTIONS/SESSION3/Exercise3_1.c
+++ EXAMQUESTIONS/SESSION3/Exercise3_1.c
@@ -8,14 +8,16 @@ int search(int *v1, int *v2, int d1, int d2);
 
 int main()
 {
-    int v1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int v2 = {4, 5, 6, 7};
+    int v1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int v2[4] = {4, 5, 6, 7};
     int index;
 
-    index = search(v1, v2, sizeof(v1), sizeof(v2));
+    index = search(&v1, &v2, sizeof(v1), sizeof(v2));
+    printf("%d", index);
     return 0;
 }
 
+/*solve again*/
 int search(int *v1, int *v2, int d1, int d2)
 {
     char *p1, *p2;
@@ -23,14 +25,12 @@ int search(int *v1, int *v2, int d1, int d2)
 
     for (int i = 0; i < d1; i++)
     {
-        if (*p1 == *(p2 + i))
+        printf("%d", v1[i]);
+        if (v1[i] == v2[0])
         {
             index = i;
             break;
         }
-
-        if (*(p2 + i) == '/0')
-            return -1;
     }
 
     return index;
