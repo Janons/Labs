8048b7e4e3f4069114658f420eca62012619fd2b
Can Dogrul <candogrull@hotmail.com>
2023-12-27 20:23:04 +0300

    modified:   EXAMQUESTIONS/SESSION1/Question3.c 	renamed:    EXAMQUESTIONS/SESSION 2/.vscode/tasks.json -> EXAMQUESTIONS/SESSION2/.vscode/tasks.json 	renamed:    EXAMQUESTIONS/SESSION 2/Session2_1 -> EXAMQUESTIONS/SESSION2/Session2_1 	renamed:    EXAMQUESTIONS/SESSION 2/Session2_1.c -> EXAMQUESTIONS/SESSION2/Session2_1.c 	renamed:    EXAMQUESTIONS/SESSION 2/Session2_1.dSYM/Contents/Info.plist -> EXAMQUESTIONS/SESSION2/Session2_1.dSYM/Contents/Info.plist 	renamed:    EXAMQUESTIONS/SESSION 2/Session2_1.dSYM/Contents/Resources/DWARF/Session2_1 -> EXAMQUESTIONS/SESSION2/Session2_1.dSYM/Contents/Resources/DWARF/Session2_1 	renamed:    EXAMQUESTIONS/SESSION 2/Session2_2.c -> EXAMQUESTIONS/SESSION2/Session2_2.c 	renamed:    EXAMQUESTIONS/SESSION 2/Session_2_2.h -> EXAMQUESTIONS/SESSION2/Session_2_2.h

--- EXAMQUESTIONS/SESSION1/Question3.c
+++ EXAMQUESTIONS/SESSION1/Question3.c
@@ -1,11 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int backtrack()
+char  backtrack(int *stringArray, int *mark, int *sol,int n, int l, int r);
+void swap(char *x, char *y);
+
+int main(int argc, char *argv[])
 {
+    if (argc < 6)
+    {
+        fprintf(stdout, "Insufficient amount of arguments");
+        return EXIT_FAILURE;
+    }
+
+    char arr[5] = {argv[1], argv[2], argv[3], argv[4], argv[5]};
+
+    return 0;
 }
 
-int main()
+void swap(char *x, char *y)
 {
-    return 0;
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+char backtrack(int *stringArray, int *mark, int *sol,int n, int l, int r,
+int pos)
+{
+    int i;
+    if (pos >= n)
+    {
+        return;
+    }
+
+    for (i = l; i < r; i++)
+    {
+        if (mark[i] == 0)
+        {
+            swap(string[i+l][i+r])
+        }
+    }
+    return;
 }
