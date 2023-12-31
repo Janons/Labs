ea728cb4b81e0ce32ec9a0dd3f3062b2fc50a5ae
Can Dogrul <candogrull@hotmail.com>
2023-12-31 21:49:42 +0300

    modified:   EXAMQUESTIONS/SESSION1/Question1.c

--- EXAMQUESTIONS/SESSION1/Question1.c
+++ EXAMQUESTIONS/SESSION1/Question1.c
@@ -4,46 +4,39 @@
 int *merge3(int *a, int *b, int *c, int na, int nb, int nc);
 int main()
 {
-
 }
 
 int *merge3(int *a, int *b, int *c, int na, int nb, int nc)
 {
-    int *d;
-    int *m[3], len[3];
+    int *d, len[3], sum;
+    char *m[3];
 
     m[0] = a;
     m[1] = b;
     m[2] = c;
 
-    len[0] = na;
-    len[1] = nb;
-    len[2] = nc;
+    len[1] = na;
+    len[2] = nb;
+    len[3] = nc;
 
-    int sum = na + nb + nc;
+    sum = na + nb + nc;
 
-    d = (int *)malloc(sum * sizeof(int));
-    if (d == NULL)
+    for (int i = 0; i < sum; i++)
     {
-        perror("Error in memory allocating");
-        return EXIT_FAILURE;
-    }
-
-    for (int i; i < sum; i++)
-    {
+        /*whe a minimum canditate hasn't been found yet*/
         int min_index = -1;
-        for (int j = 0; j < 3; j++)
+        /*algorithms*/
+
+        for (int j; j < sum; j++)
         {
-            if (len[j] != 0 || (min_index == -1 || *m[min_index] > *m[j]))
+            if (len[j] != 0 && (min_index == -1 || *m[min_index] > *m[j]))
             {
                 min_index = j;
             }
         }
 
-        d[i] = *m[min_index]; // we access the value with the pointers
+        d[i] = *m[min_index];
         m[min_index]++;
         len[min_index]--;
     }
-
-    return d;
 }
