252925cb0f04b9954d14ffe2381afc32a8be2b27
Can Dogrul <candogrull@hotmail.com>
2023-12-31 21:24:31 +0300

    new file:   EXAMQUESTIONS/SESSION2/Session2_3 	modified:   EXAMQUESTIONS/SESSION2/Session2_3.c 	new file:   EXAMQUESTIONS/SESSION2/Session2_3.dSYM/Contents/Info.plist 	new file:   EXAMQUESTIONS/SESSION2/Session2_3.dSYM/Contents/Resources/DWARF/Session2_3

--- EXAMQUESTIONS/SESSION2/Session2_3.c
+++ EXAMQUESTIONS/SESSION2/Session2_3.c
@@ -14,12 +14,14 @@ int main(int argc, char *argv[])
     int taken[N] = {0};
     int n_char;
 
-    if (argc < 0)
+    if (argc < 2)
     {
         fprintf(stderr, "Insufficient amount of arguments\n");
         exit(EXIT_FAILURE);
     }
     n_char = atoi(argv[1]);
+
+    fprintf(stdout, "%d", n_char);
     if (n_char < N)
     {
         fprintf(stderr, "Number of characters must be equal or bigger than characters in the string");
@@ -31,6 +33,9 @@ int main(int argc, char *argv[])
     {
         exit(EXIT_FAILURE);
     }
+
+    arr(sol, alfa, taken, 0, n_char - N, n_char);
+    free(sol);
 }
 
 void arr(char *sol, char alfa[], int taken[], int position, int reps_allowed, int n_char)
@@ -39,6 +44,12 @@ void arr(char *sol, char alfa[], int taken[], int position, int reps_allowed, in
 
     if (position == n_char)
     {
+
+        for (i = 0; i < n_char; i++)
+        {
+            fprintf(stdout, "%c", sol[i]);
+        }
+        fprintf(stdout, "\n");
         return;
     }
 
@@ -48,6 +59,16 @@ void arr(char *sol, char alfa[], int taken[], int position, int reps_allowed, in
         {
             taken[i] = 1;
             sol[position] = alfa[i];
+            arr(sol, alfa, taken, position + 1, reps_allowed, n_char);
+            taken[i] = 0;
+        }
+        else
+        {
+            if (taken[i] && reps_allowed > 0)
+            {
+                sol[position] = alfa[i];
+                arr(sol, alfa, taken, position + 1, reps_allowed - 1, n_char);
+            }
         }
     }
 }
