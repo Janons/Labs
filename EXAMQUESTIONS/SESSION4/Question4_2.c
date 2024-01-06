2c9d7490e442e8c3ed5da3795a54bac504fa4c1a
Can Dogrul <candogrull@hotmail.com>
2024-01-06 22:03:11 +0300

    modified:   EXAMQUESTIONS/SESSION4/Question4_2.c

--- EXAMQUESTIONS/SESSION4/Question4_2.c
+++ EXAMQUESTIONS/SESSION4/Question4_2.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*definitions*/
+#define USESTRCPY 1
+
 /*struct*/
 
 typedef struct node
@@ -22,24 +25,34 @@ int main()
 
 node_t *string_split(char *str)
 {
-    node_t *newNode, *top;
+    node_t *strHolder, *top;
 
     int starting_index, ending_index;
 
-    int lenghtOftheString = (sizeof(str) % sizeof(str[0]));
-    char *strHolder;
-
-    while (starting_index < lenghtOftheString || ending_index < lenghtOftheString)
+    while (ending_index < strlen(str))
     {
 
-        while (str[ending_index] != '.' && str[ending_index] == str[starting_index])
+        while (ending_index < strlen(str) && str[ending_index] != '.')
         {
             ending_index++;
         }
 
-        strHolder = (char *)malloc((ending_index - starting_index) * sizeof(char));
-
-       
+        strHolder = (node_t *)malloc(sizeof(node_t));
+        if (strHolder == NULL)
+        {
+            fprintf(stdout, "Error in allocating memory for the string holder");
+            return EXIT_FAILURE;
+        }
+        strHolder->str = malloc((ending_index - starting_index + 1) * sizeof(char));
     }
-}
 
+#if USESTRCPY
+    strncpy(strHolder->str, &str[starting_index], (ending_index - starting_index));
+
+#endif
+
+    strHolder->next = top;
+    top = strHolder;
+
+    return top;
+}
