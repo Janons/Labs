ab64c1486cac8c6a665d9bb49435539fef0ab8c3
Can Dogrul <candogrull@hotmail.com>
2024-01-08 11:50:58 +0300

    modified:   EXAMQUESTIONS/SESSION4/Question4_2.c

--- EXAMQUESTIONS/SESSION4/Question4_2.c
+++ EXAMQUESTIONS/SESSION4/Question4_2.c
@@ -9,27 +9,28 @@
 
 typedef struct node
 {
-    char *str;
+    char *str1;
     struct node *next;
 
 } node_t;
+
 /*prototypes*/
 node_t *string_split(char *str);
-char *split_string(char *str);
-node_t *createNode();
 
 int main()
 {
+
     return 0;
 }
 
 node_t *string_split(char *str)
 {
-    node_t *strHolder, *top;
+    node_t *top, *node;
 
     int starting_index, ending_index;
 
-    while (ending_index < strlen(str))
+    starting_index = 0, ending_index = 0;
+    while (starting_index < strlen(str))
     {
 
         while (ending_index < strlen(str) && str[ending_index] != '.')
@@ -37,22 +38,24 @@ node_t *string_split(char *str)
             ending_index++;
         }
 
-        strHolder = (node_t *)malloc(sizeof(node_t));
-        if (strHolder == NULL)
+        /*memory allocation for the new node*/
+        node = (node_t *)malloc(1 * sizeof(node_t)); // include the one
+        if (node == NULL)
         {
-            fprintf(stdout, "Error in allocating memory for the string holder");
+            fprintf(stdout, "Error in allocating memory for the node");
             return EXIT_FAILURE;
         }
-        strHolder->str = malloc((ending_index - starting_index + 1) * sizeof(char));
+        node->str1 = malloc((ending_index - starting_index) * sizeof(node_t));
     }
-
 #if USESTRCPY
-    strncpy(strHolder->str, &str[starting_index], (ending_index - starting_index));
+
+    strncpy(node->str1, &str[starting_index], (ending_index - starting_index));
 
 #endif
 
-    strHolder->next = top;
-    top = strHolder;
+    /*insertion to top*/
+    node->next = top;
+    top = node;
 
     return top;
 }
