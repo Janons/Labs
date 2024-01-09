34c1cf6ded69c0c3d808bd742c83a5b99d28c37f
Can Dogrul <candogrull@hotmail.com>
2024-01-09 14:19:09 +0300

    new file:   EXAMQUESTIONS/SESSION5/Session5_3.c

---
+++ EXAMQUESTIONS/SESSION5/Session5_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*prototype*/
+
+
+int main(int argc, char *argv)
+{
+
+    if (argc < 2)
+    {
+        return EXIT_FAILURE;
+    }
+}
