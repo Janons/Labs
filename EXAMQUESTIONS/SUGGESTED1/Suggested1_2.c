99c904de70ec2d59d59c4799e20e6da59cdd91e1
Can Dogrul <candogrull@hotmail.com>
2024-01-16 19:17:58 +0100

    modified:   EXAMQUESTIONS/SUGGESTED1/Suggested1_2.c

--- EXAMQUESTIONS/SUGGESTED1/Suggested1_2.c
+++ EXAMQUESTIONS/SUGGESTED1/Suggested1_2.c
@@ -2,8 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct element
+{
+    int key;
+
+    struct element *next;
+};
+void parse_and_insert(char *s, struct element **p);
+void createNode();
+
 int main()
 {
 
     return 0;
+}
+
+void parse_and_insert(char *s, struct element **p)
+{
+    char *token;
+    struct element *node;
+
+    /*get the first token*/
+    token = strtok(s, " ");
+
+    /*insert the first token*/
+    
 }
