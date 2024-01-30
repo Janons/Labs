aa8a26f6c54b1a158948ce93abc2176f1068c369
Can Dogrul <candogrull@hotmail.com>
2024-01-30 21:48:32 +0100

    modified:   EXAMQUESTIONS/SESSION8/Session8_3.c

--- EXAMQUESTIONS/SESSION8/Session8_3.c
+++ EXAMQUESTIONS/SESSION8/Session8_3.c
@@ -6,7 +6,8 @@ const int y_offset = {-1, 0, 1, 0};
 
 int util_malloc(int size);
 int create_matrix(int **matrix); // opens the matrix
-void recurBestsum(int **matrix, int matsize, int r, int c, int bestSum, int sum, int *mark);
+void recurBestsum(int **matrix, int matsize, int r, int c, int *marked, int *markedBest, int *pathSumMax, int pathsumCurrent, int *pathLenMax,
+int pathLenCurrent);
 
 int main()
 {
