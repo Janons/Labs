de0dbb36e9599eb69a70a6ea10a2b20a1819760f
Can Dogrul <candogrull@hotmail.com>
2024-01-30 21:46:49 +0100

    new file:   EXAMQUESTIONS/SESSION8/Session8_3.c

---
+++ EXAMQUESTIONS/SESSION8/Session8_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int x_offset = {0, 1, 0, -1};
+const int y_offset = {-1, 0, 1, 0};
+
+int util_malloc(int size);
+int create_matrix(int **matrix); // opens the matrix
+void recurBestsum(int **matrix, int matsize, int r, int c, int bestSum, int sum, int *mark);
+
+int main()
+{
+    return 0;
+}
