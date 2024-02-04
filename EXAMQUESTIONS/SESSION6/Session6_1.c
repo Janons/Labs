0920b2fdf851992204653a245715e1d04f6c8f30
Can Dogrul <candogrull@hotmail.com>
2024-02-04 18:28:16 +0100

    modified:   EXAMQUESTIONS/SESSION6/Session6_1.c

--- EXAMQUESTIONS/SESSION6/Session6_1.c
+++ EXAMQUESTIONS/SESSION6/Session6_1.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void sequence_invert(int *v1, int n, int *v2);
+
 int main()
 {
     return 0;
+}
+
+void sequence_invert(int *v1, int n, int *v2)
+{
+    int i, j, k;
+
+    for (i = 0, j = 1; j < n; i++, j++)
+    {
+        if (v1[j] - v1[i] = 1)
+        {
+            for (k = 0; k < n - j || ; k++)
+            {
+                
+
+            }
+        }
+    }
 }
