c1eba0c364fe54ff435b622a896a1fb3a1f4aba6
Can Dogrul <candogrull@hotmail.com>
2024-02-05 13:31:40 +0100

    modified:   EXAMQUESTIONS/SESSION6/Session6_2.c 	modified:   EXAMQUESTIONS/SESSION8/Session8.c

--- EXAMQUESTIONS/SESSION6/Session6_2.c
+++ EXAMQUESTIONS/SESSION6/Session6_2.c
@@ -9,9 +9,25 @@ typedef struct node_s
 } node_t;
 
 void pathwrite(node_t *root, int height);
-void pathwrite_recursion(node_t *root, );
+void pathwrite_recursion(node_t *root, int height, int level);
 
 int main()
 {
     return 0;
+}
+void pathwrite(node_t *root, int height)
+{
+    /*we create the path tree here*/
+}
+
+void pathwrite_recursion(node_t *root, int height, int level)
+{
+
+    /*base case*/
+    if (root == NULL)
+    {
+        return;
+    }
+
+    printf("%d", root->right);
 }
--- EXAMQUESTIONS/SESSION8/Session8.c
+++ EXAMQUESTIONS/SESSION8/Session8.c
@@ -15,40 +15,20 @@ void bishop_placer(int **matrix, int n) // assuming that its a square matrix
     int r, c, r_b, c_b;
     int max, sum, rbest, cbest;
 
+    /*iterarting over the outer loops*/
     for (r = 0; r < n; r++)
     {
         for (c = 0; c < n; c++)
         {
+            /*checking whether we have found the zero*/
             if (matrix[r][c] == 0)
             {
-
-                // upper-left search
-
-                for (r_b = r, c_b = c; c_b >= 0, r_b >= c; r_b--, c_b--)
-                {
-                    sum += matrix[r_b][c_b];
-                }
-
-                // upper-right search
-
-                for (r_b = r, c_b = c; r_b >= 0, c_b <= n; r_b--, c_b++)
-                {
-                    sum += matrix[r_b][c_b];
-                }
-
-                // left-down search
-                for (r_b = r, c_b = c; r_b <= n, c_b >= 0; r_b++, c_b--)
-                {
-                    sum += matrix[r_b][c_b];
-                }
-
-                // left-right search
-                for (r_b = r, c_b = c; r_b <= n, c_b <= n; r_b++, c_b++)
+                /*checking rigt-upper part of the matrix*/
+                for (r_b = r, c_b = c; r_b < 0, c_b < n; r_b--, c_b++)
                 {
                     sum += matrix[r_b][c_b];
                 }
             }
         }
     }
-}
 }
