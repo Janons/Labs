e774bf8d7d20d1da40315aea40b7d7ea2dcee927
Can Dogrul <candogrull@hotmail.com>
2024-02-09 12:47:59 +0100

    modified:   EXAMQUESTIONS/SESSION6/Session6_1.c 	new file:   EXAMQUESTIONS/SESSION9/Session9_1.c

--- EXAMQUESTIONS/SESSION6/Session6_1.c
+++ EXAMQUESTIONS/SESSION6/Session6_1.c
@@ -8,23 +8,32 @@ int main()
     return 0;
 }
 
-void sequence_invert(int *v1, int n, int *v2)
+void seqnence_invert(int *v1, int n, int *v2)
 {
     int i, j, k;
 
-    // we track everything with k
+    /*get into the list*/
 
     for (i = 0, k = 1; k < n; k++)
     {
-        /*checking end of the subsequence*/
+        /*search whether we have reach the
+        end of a sequence*/
+
         if (v1[k] <= v1[k - 1])
         {
+            /*start the reversing process*/
+
             for (j = 0; j < (k - i); j++)
             {
-
-                v2[i + j] = v1[k - i - 1];
+                v2[j] = v2[k - j - 1];
             }
+            i = k;
         }
-        i = k;
+
+        // deal with the last sub-sequence
+        for (j = 0; j < (k - i); j++)
+        {
+            v2[j] = v1[k - j - 1];
+        }
     }
 }
